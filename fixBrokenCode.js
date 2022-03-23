7e9f132b2549e744191c1cf43e68d75a524f477b
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-03-23 08:49:09 -0400

    Create fixBrokenCode.js

---
+++ fixBrokenCode.js
@@ -0,0 +1,12 @@
+/*================================================
+This program doesn't work: 
+function multiply(a, b){
+  a * b
+}
+
+Figure out why
+================================================*/
+
+function multiply(a, b){
+  return a * b
+}
