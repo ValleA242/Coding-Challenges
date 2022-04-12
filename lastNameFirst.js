4d2401c7924d6e47365efa3861f1b5266c35ab78
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-04-11 21:00:03 -0400

    Create lastNameFirst.js

---
+++ lastNameFirst.js
@@ -0,0 +1,10 @@
+/*=============================
+Write a function that returns
+a string in which firstname is
+swapped with last name.
+=============================*/
+
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
