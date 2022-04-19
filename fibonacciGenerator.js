a59590022a2a277bbe0c683d08b3db02dfbbc80e
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-04-19 15:26:06 -0400

    Update fibonacciGenerator.js

--- fibonacciGenerator.js
+++ fibonacciGenerator.js
@@ -2,15 +2,16 @@ function fibonacciGenerator (n) {
     
     let fiboArr = []
     
-    if(n === 1){
+    if(n === 1 || n === 0){
         fiboArr = [0]
+        return fiboArr
     } else if(n === 2){
         fiboArr = [0, 1]
+        return fiboArr
     } else {
         fiboArr = [0, 1]
         for(let i = 1; i < n; i++){
-               fiboArr.push(fiboArr[i] + fiboArr[i - 1])
-            }
+         fiboArr.push(fiboArr[i] + fiboArr[i - 1])
         }
     }
     return fiboArr
