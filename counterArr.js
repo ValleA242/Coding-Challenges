83bb3a4d6ca73c343ae35a5d79d6d9b3462bcb06
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-04-19 12:44:41 -0400

    Create counterArr.js

---
+++ counterArr.js
@@ -0,0 +1,11 @@
+// Create a function that counts from one and inserts the count into an array
+
+
+let output = [];
+let count = 1
+
+function fizzbuzz(){
+  output.push(count)
+  count ++;
+  console.log(output)
+}
