ed1ccd44fe521dc82c872a8b0bc59ab1a0c2ba47
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-04-24 14:04:17 -0400

    Create splitNumReverse.js

---
+++ splitNumReverse.js
@@ -0,0 +1,8 @@
+/*========================================================
+Given a random non-negative number, you have to return the
+digits of this number within an array in reverse order.
+========================================================*/
+
+function digitize(n) {
+  return reverseStringArray = n.toString().split('').reverse().map(x => parseInt(x))
+}
