f9d208c9bd64e9069ef38cf3417fe89d1bbd91b5
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-05-11 12:32:14 -0400

    Create accumulateLetters.js

---
+++ accumulateLetters.js
@@ -0,0 +1,24 @@
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+
+function accum(s) {
+   s = s.toUpperCase();
+  let solutionArr = []
+  let getArr = s.split('')
+  let getArrLetters = getArr.map((ele) => {
+    return [ele]})
+  for(let i = 0; i < getArr.length; i++){
+    if(i === 0){
+      solutionArr.push(getArr[0] + '-')
+    } else {
+    for(let j = 0; j <= i; j++){
+        if(j === 0){
+      solutionArr.push(getArr[i])
+    } else if(j === i && i !== getArr.length - 1) {
+          solutionArr.push(getArr[i].toLowerCase() + '-')
+      } else {
+          solutionArr.push(getArr[i].toLowerCase())
+        }
+      } 
+    } 
+  } return solutionArr.join('')
+}
