fc10888b8e5aa96dec91767f6f8d4480c830be3e
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-05-19 13:02:45 -0400

    Create sqrEach.js

---
+++ sqrEach.js
@@ -0,0 +1,15 @@
+/*==============================================
+Welcome. In this kata, you are asked to square 
+every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function,
+811181 will come out, because 92 is 81 and 12 is 1.
+==============================================*/
+
+function squareDigits(num){
+  let numArr = Array.from(String(num),Number)
+  let sqrArr = numArr.map(x => 
+    x * x
+  );
+   return Number(sqrArr.join(''))
+}
