999c9168d310b6f23ce4027dd3a96351148d594d
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-05-21 17:48:59 -0400

    Update alphabetPosition.js

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -1,10 +1,6 @@
 /*==========================================================
 In this kata you are required to, given a string, replace 
 every letter with its position in the alphabet.
-
-If anything in the text isn't a letter, ignore it and don't 
-return it.
-
 ==========================================================*/
 
 function alphabetPosition(text) {
