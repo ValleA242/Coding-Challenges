66a789c130a6d0f942da5c7ee44592947be199c0
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-05-27 16:29:43 -0400

    Update simpleReturn.js

--- simpleReturn.js
+++ simpleReturn.js
@@ -1,5 +1,5 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function boolToWord( bool ){
-  return bool ? 'Yes':'No'
+  return bool ? 'Yes':'No' //if true return 'Yes' if not return 'No'
 }
