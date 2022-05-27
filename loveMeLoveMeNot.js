549e23ad6523748c035540938f7fa9769a68f68e
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-05-26 21:58:39 -0400

    Create loveMeLoveMeNot.js

---
+++ loveMeLoveMeNot.js
@@ -0,0 +1,27 @@
+/*
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+*/
+
+function howMuchILoveYou(nbPetals) {
+  const outcomes = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+  let count = 0
+  for(let i = 0; i < 6; i++){
+    ++count
+    if(count === nbPetals){
+      return outcomes[i]
+    }
+    if(i === 5){
+      i = -1
+    }
+  }
+}
