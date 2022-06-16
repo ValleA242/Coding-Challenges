429d69147a231ee952c51b0d0955a8acddca74ee
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-06-15 21:54:39 -0400

    Update divisors.js

--- divisors.js
+++ divisors.js
@@ -1,11 +1,11 @@
 //Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
 
 function divisors(integer) {
-  let divisors = []
-  for(let i = 2; i < integer; i++){
-    if(integer % i === 0){
+  let divisors = [] //set array to push into 
+  for(let i = 2; i < integer; i++){ 
+    if(integer % i === 0){ //if i divides evenly into integer i is a divisor and will be pushed into the divisors array
      divisors.push(i)
     }
-  } return divisors.length === 0 ? `${integer} is prime` : divisors  
+  } return divisors.length === 0 ? `${integer} is prime` : divisors  //return the array of divsors. If the number is prime return the set string 
 };
 
