af35177b47b3725322a650df9846b6d829da8997
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2023-10-11 21:30:46 -0400

    Create missingLetter.js
    Established initial variables and iteration. Will continue tomorrow.

---
+++ missingLetter.js
@@ -0,0 +1,15 @@
+/*
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+*/
+
+function findMissingLetter(array){
+ var array = array.toUpperCase();
+  var alphabetReference = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var startingPoint = alphabetReference.indexOf(array[0])
+  for (i = 0; i < array.length; i++){
+    
+  }
+}
