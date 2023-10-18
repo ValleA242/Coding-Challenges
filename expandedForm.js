b9e85b72d2681a2d193182060675e55d3292fbb6
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2023-10-17 22:57:51 -0400

    Create expandedForm.js
    Need to circle back and omit place holding zeros populating in the final string.

---
+++ expandedForm.js
@@ -0,0 +1,29 @@
+/* In Progress */
+
+/* You will be given a number and you will need to r
+eturn it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+
+*/
+
+function expandedForm(num) {
+let numArray = num.toString().split('');
+let numPlaceArray = [];
+
+    for(let i = numArray.length; i > 0; i--){
+        if(i != 0){
+            numPlaceArray.push(Math.pow(10, i - 1))
+        } else {
+            numPlaceArray.push(1)
+        }
+    }
+    numPlaceArray = numPlaceArray.toString().split(',');
+  console.log(numArray)
+    
+ let newArray = numArray.map((x, index) => x * numPlaceArray[index]);
+    
+    return newArray.toString().replaceAll(',', ' + ').replaceAll(' 0 ', '').replaceAll('++', '+');
+}
