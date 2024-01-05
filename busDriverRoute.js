a9e89c96febdf91c9c24a9f40381fae5a78dfa02
Alicio Valle <alicio.valle242@gmail.com>
2024-01-05 08:30:34 -0500

    added function for challenge

--- busDriverRoute.js
+++ busDriverRoute.js
@@ -12,3 +12,6 @@
 // cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
+const busRoute = (cap, on, wait) => {
+    
+}
