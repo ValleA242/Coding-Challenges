30caff93561af7ac63149da52581c04ce9518928
Alicio Valle <alicio.valle242@gmail.com>
2024-03-12 21:11:14 -0400

    Panagram Challenge

---
+++ panagram.js
@@ -0,0 +1,25 @@
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string){
+    let alphabetArray = [];
+    let comparisonArray = [];
+
+  for(let i = 0; i < 26; i++) {
+      alphabetArray.push(String.fromCharCode(97 + i));
+  }
+    const charArray = string.toLowerCase().split('')
+    
+    charArray.map(function(letter, index){
+       
+        for(let i = 0; i < alphabetArray.length; i++){
+            if(alphabetArray[i] === (letter) && !comparisonArray.includes(letter)){
+                comparisonArray.push(letter);
+            }
+        }
+    })
+    return comparisonArray.length < 26 ? false : true;
+  }
+
+  console.log(isPangram(''))
