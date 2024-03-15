b46d06ba13842382ef3f512d47e3c62d40d4155b
Alicio Valle <alicio.valle242@gmail.com>
2024-03-14 23:54:51 -0400

    corrected comment

--- billboardPricing.js
+++ billboardPricing.js
@@ -10,6 +10,7 @@ You can not use multiplier "*" operator.
 If your name would be Jeong-Ho Aristotelis,
 ad would cost £600. 20 leters * 30 = 600 
 (Space counts as a letter).
+----
 */
 
 function billboard(name, price = 30){
