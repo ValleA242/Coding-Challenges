ef662238351699fbc9625c5bd822d1c5fe41e9a6
AJValle <71409214+ValleA242@users.noreply.github.com>
2024-12-06 14:51:12 -0500

    Leetcode entry

--- addLinkedListNums.js
+++ addLinkedListNums.js
@@ -7,3 +7,21 @@ function ListNode(val, next = null) {
     this.next = next;
 }
 
+function addTwoNumbers(l1, l2) {
+    const dummy = new ListNode(0);
+    let tail = dummy;
+    let carry = 0;
+
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        const x = l1 ? l1.val : 0;
+        const y = l2 ? l2.val : 0;
+
+        const sum = x + y + carry;
+        carry = Math.floor(sum / 10);
+        const digit = sum % 10;
+
+        tail.next = new ListNode(digit);
+        tail = tail.next;
+    }
+}
