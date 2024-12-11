a568aca40532618aebd896d0f320add9527fb0a2
AJValle <71409214+ValleA242@users.noreply.github.com>
2024-12-10 20:28:46 -0500

    Leetcode entry

---
+++ longestSubString.js
@@ -0,0 +1,6 @@
+var lengthOfLongestSubstring = function (s) {
+    const subStringObj = new Map(); //creates new array when called
+    const stringArray = s.split('');
+
+    for (let i = 0; i < s.length)
+};
